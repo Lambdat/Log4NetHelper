77e343013bee52690158865645a92d73b31718e8
Lambdat <oreficeantonio98@postecert.it>
2021-12-06 09:15:54 +0100

    Aggiungere i file di progetto.

---
+++ Log4NetHelper.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using log4net.Config;
+using log4net.Repository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogHelper
+{
+    public class Log4NetHelper
+    {
+        public static ILog GetLogger([CallerFilePath] string fileName = "") // il parametro filename è il nome della classe alla quale vogliamo aggiungere il log4net
+        {                                                                    //Esso è un parametro opzionale nel caso avessimo più classi all'interno di un file.cs , se cosi non fosse andrebbe bene lasciarlo di default
+            ILoggerRepository iLogRepo = LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+            XmlConfigurator.Configure(iLogRepo, new FileInfo("log4net.config")); // Andiamo a specificare il file.xml dove prendere le configurazioni del logger
+          
+            return LogManager.GetLogger(fileName);   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Text;
+using log4net;
+
+ILog log = LogHelper.Log4NetHelper.GetLogger();
+
+string me = "sono Antonio!"; 
+
+log.Info(String.Format("Ciao Mondo, {0}",me));
+
+try
+{
+    int divisore = 0;
+
+    Console.WriteLine("Inserisci un dividendo: \n");
+
+    int dividendo = int.Parse(Console.ReadLine());
+
+    decimal divisione = dividendo / divisore;
+
+}catch(DivideByZeroException ex)
+{
+    log.Error(new StringBuilder("Eccezione rilevata : ").Append(ex.Message).ToString());
+
+}
+
